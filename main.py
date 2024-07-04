440344ae31cb77afba18b4a7764ceb5969bc26b8
Tanmay Patil <tanmay.patil14@gmail.com>
2024-07-04 20:47:45 +0530

    WIP: cli with simple positional argument for fname

--- main.py
+++ main.py
@@ -14,36 +14,9 @@ class WelcomeScreen():
     def __init__(self):
         # init cli 
         print("CAMS 2 CSV CLI version")
-        # super(WelcomeScreen, self).__init__()
-        # loadUi(os.path.join(basedir,"welcome.ui"), self)
-        # self.btn_browse.clicked.connect(self.file_dailog)
-        # self.chk_password.toggled.connect(self.enable_pw_input)
-        # self.btn_submit.clicked.connect(self.process_thread)
 
 
-    def file_dailog(self):
-        self.clear_fields()
-        filename, _ = QFileDialog.getOpenFileName(
-            parent=self,
-            caption="Select your CAMS PDF file",
-            directory=os.getcwd(),
-            filter="pdf(*.pdf)",
-        )
-        self.lbl_path.setText(filename)
-
-    def enable_pw_input(self):
-        if self.chk_password.isChecked() == True:
-            self.le_pwd.setEnabled(True)
-            self.le_pwd.setPlaceholderText("Document Password")
-            self.lbl_message.clear()
-        else:
-            self.le_pwd.setEnabled(False)
-            self.le_pwd.setPlaceholderText("")
-            self.le_pwd.clear()
-
-    def file_processing(self):
-        file_path = self.lbl_path.text()
-        doc_pwd = self.le_pwd.text()
+    def file_processing(self, file_path, doc_pwd):
         final_text = ""
 
         if not len(file_path) == 0:
@@ -57,9 +30,9 @@ class WelcomeScreen():
                 self.extract_text(final_text)
 
             except:
-                self.lbl_message.setText("Encrypted file, please enter your password")
+                print("Encrypted file, please enter your password\n")
         else:
-            self.lbl_message.setText("Please select your CAMS PDF file..")
+            print("Please select your CAMS PDF file..\n")
 
     def extract_text(self, doc_txt):
         # Defining RegEx patterns
@@ -70,6 +43,7 @@ class WelcomeScreen():
             r"(^\d{2}-\w{3}-\d{4})(\s.+?\s(?=[\d(]))([\d\(]+[,.]\d+[.\d\)]+)(\s[\d\(\,\.\)]+)(\s[\d\,\.]+)(\s[\d,\.]+)"
         )  # Extracting Transaction data
 
+        # Represents a row in all txn csv
         line_itms = []
         for i in doc_txt.splitlines():
             if fund_name.match(i):
@@ -87,7 +61,10 @@ class WelcomeScreen():
                 price = txt.group(5)
                 unit_bal = txt.group(6)
                 line_itms.append(
-                    [folio, fun_name, date, description, amount, units, price, unit_bal]
+                    [
+                        folio, fun_name, date, description, amount,
+                        units, price, unit_bal
+                    ]
                 )
 
             df = DataFrame(
@@ -113,17 +90,18 @@ class WelcomeScreen():
             df.Price = df.Price.astype("float")
             df.Unit_balance = df.Unit_balance.astype("float")
 
-            file_name = f'CAMS_data_{datetime.now().strftime("%d_%m_%Y_%H_%M")}.csv'
-            save_file = os.path.join(os.path.expanduser("~"), "Downloads", file_name)
+            date_str = datetime.now().strftime("%d_%m_%Y_%H_%M")
+            file_name = f'CAMS_data_{date_str}.csv'
+            save_file = os.path.join(".","all-txn-"+ file_name +".csv")
 
             try:
                 df.to_csv(save_file, index=False)
-                self.lbl_message.setText(
-                    "Process completed, file saved in Downloads folder"
+                print(
+                    "Process completed, file saved in current dir"
                 )
 
             except Exception as e:
-                self.lbl_message.setText(e)
+                sys.stderr.write(str(e))
 
     def clean_txt(self, x):
         x.replace(r",", "", regex=True, inplace=True)
@@ -131,28 +109,6 @@ class WelcomeScreen():
         x.replace(r"\)", " ", regex=True, inplace=True)
         return x
 
-    def clear_fields(self):
-        self.lbl_message.clear()
-        self.lbl_path.clear()
-        self.chk_password.setChecked(False)
-
-    def process_thread(self):
-        self.lbl_message.setText("Processing please wait...")
-        threading.Thread(target=self.file_processing).start()
-
-
-# Main
-# app = QApplication(sys.argv)
-# widget = WelcomeScreen()
-# widget.setWindowTitle(" ")
-# widget.setWindowIcon(QtGui.QIcon(os.path.join(basedir, "icons", "app_icon.svg")))
-# widget.show()
-
-# try:
-#     sys.exit(app.exec_())
-
-# except:
-#     print("exiting")
 
 def parse_args():
     parser = argparse.ArgumentParser(
@@ -162,9 +118,10 @@ def parse_args():
     return parser.parse_args()
 
 def main():
-    print("Init")
     args = parse_args()
     print(args.filename)
+    processor = WelcomeScreen()
+    processor.file_processing(args.filename,"")
 
 if __name__ == "__main__":
     main()
